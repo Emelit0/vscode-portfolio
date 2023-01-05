09e02dc15597ddee1d66fc798752bba75533b835
Emelit0 <stollk811@gmail.com>
2023-01-05 15:29:11 +0100

    add titlebar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,10 @@
+'use client'
+
 import './globals.css'
+import Image from 'next/image'
+import RemoveOutlinedIcon from '@mui/icons-material/RemoveOutlined';
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+
 
 export default function RootLayout({
   children,
@@ -12,7 +18,26 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <section className='titleBar'>
+          <Image src='../public/vscode.svg' alt='vscode-icon' width={42} height={42}/>
+          <p>File</p> 
+          <p>Edit</p>
+          <p>View</p>
+          <p>Go</p>
+          <p>Run</p>
+          <p>Terminal</p>
+          <p>Help</p>
+          <div className="title">
+            Kevin Stoll - Visual Studio Code
+          </div>
+          <div className="window-btns">
+            <RemoveOutlinedIcon />
+            <Image src='../public/window-restoredown.svg' alt='window-restoredown' width={42} height={42}/>
+            <CloseOutlinedIcon />
+          </div>
+        </section>
+        {children}</body>
     </html>
   )
 }
