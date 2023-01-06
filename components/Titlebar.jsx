0fa0c28252890f9e0a2067360dea35b16ab70da5
Emelit0 <stollk811@gmail.com>
2023-01-06 11:41:53 +0100

    add titlebar component

---
+++ components/Titlebar.jsx
@@ -0,0 +1,41 @@
+'use client'
+
+import Image from 'next/image'
+// import RemoveOutlinedIcon from '@mui/icons-material/RemoveOutlined';
+// import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import styles from '../styles/Titlebar.module.css'
+
+
+const  Titlebar = () => {
+    return (
+        <section className={styles.titlebar}>
+          <Image 
+          src='/vscode.svg' 
+          alt='vscode-icon' 
+          width={16} 
+          height={16} 
+          className={styles.vsicon}
+          />
+          <div className={styles.navbar_items}>
+          <p>File</p> 
+          <p>Edit</p>
+          <p>View</p>
+          <p>Go</p>
+          <p>Run</p>
+          <p>Terminal</p>
+          <p>Help</p>
+          </div>
+          <p className={styles.title}>Kevin Stoll - Visual Studio Code</p>
+          <div className={styles.window_btns}>
+            {/* <RemoveOutlinedIcon />
+            <Image src='../public/window-restoredown.svg' alt='window-restoredown' width={12} height={12}/>
+            <CloseOutlinedIcon /> */}
+            <span className={styles.minimize}></span>
+            <span className={styles.maximize}></span>
+            <span className={styles.close}></span>
+          </div>
+        </section>
+    ) 
+}
+
+export default Titlebar
