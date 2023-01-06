f58960b94f4eafafd43cb0477cd86fbe48f54ebd
Emelit0 <stollk811@gmail.com>
2023-01-06 11:42:25 +0100

    fix styling issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
-'use client'
 
-import './globals.css'
-import Image from 'next/image'
-import RemoveOutlinedIcon from '@mui/icons-material/RemoveOutlined';
-import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+
+import Titlebar from '../components/Titlebar.jsx'
 
 
 export default function RootLayout({
@@ -19,24 +16,7 @@ export default function RootLayout({
       */}
       <head />
       <body>
-        <section className='titleBar'>
-          <Image src='../public/vscode.svg' alt='vscode-icon' width={42} height={42}/>
-          <p>File</p> 
-          <p>Edit</p>
-          <p>View</p>
-          <p>Go</p>
-          <p>Run</p>
-          <p>Terminal</p>
-          <p>Help</p>
-          <div className="title">
-            Kevin Stoll - Visual Studio Code
-          </div>
-          <div className="window-btns">
-            <RemoveOutlinedIcon />
-            <Image src='../public/window-restoredown.svg' alt='window-restoredown' width={42} height={42}/>
-            <CloseOutlinedIcon />
-          </div>
-        </section>
+        <Titlebar />
         {children}</body>
     </html>
   )
