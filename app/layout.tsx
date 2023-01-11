bc49d66e7cbc544f982e74da1aa5efef4fe4e6a9
Emelit0 <stollk811@gmail.com>
2023-01-11 21:44:38 +0100

    edit appLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import Sidebar from "../components/Sidebar";
 import Titlebar from "../components/Titlebar";
+import Explorer from "../components/Explorer";
+import styles from "../styles/Layout.module.css";
+import Footer from "../components/Footer";
+import { style } from "@mui/system";
 
 export default function RootLayout({
   children,
@@ -15,10 +19,12 @@ export default function RootLayout({
       <head />
       <body>
         <Titlebar />
-        <div className="main">
+        <div className={styles.main}>
           <Sidebar />
+          <Explorer />
         </div>
         {children}
+        <Footer />
       </body>
     </html>
   );
