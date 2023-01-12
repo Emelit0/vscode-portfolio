c02a6a2dec9244a1ecfacb725a05d5efea95b362
Emelit0 <stollk811@gmail.com>
2023-01-12 23:43:07 +0100

    finish tab component

--- components/Tab.tsx
+++ components/Tab.tsx
@@ -4,12 +4,17 @@ import { usePathname } from "next/navigation";
 import styles from "../styles/Tab.module.css";
 import { ReactElement } from "react";
 
-const Tab: React.FC = ({ Icon, filename, path }): ReactElement => {
+const Tab: React.FC = ({ icon, filename, path }): ReactElement => {
   const pathName = usePathname();
 
-  return <Link href={path}>
-    <div className={`${styles.tab} ${pathName === path && styles.active} `}>
-        <Image src={}
-    </div>
-  </Link>;
+  return (
+    <Link href={path}>
+      <div className={`${styles.tab} ${pathName === path && styles.active} `}>
+        <Image src={icon} alt={filename} height={18} width={18} />
+        <p>{filename}</p>
+      </div>
+    </Link>
+  );
 };
+
+export default Tab;
