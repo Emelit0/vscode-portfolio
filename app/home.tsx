6204e7ec75368033a68c70c6b7e391df203edfd1
Emelit0 <stollk811@gmail.com>
2023-01-26 23:35:46 +0100

    add typwriting to homepage

---
+++ app/home.tsx
@@ -0,0 +1,19 @@
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import TypewriterComponent from "typewriter-effect";
+
+const Home: React.FC = (): ReactElement => {
+  return (
+    <div className="container">
+      <SyntaxHighlighter language="javascript" style={atomDark}>
+        <TypewriterComponent
+          options={{
+            strings: ["Hello", "World"],
+            autoStart: true,
+            loop: true,
+          }}
+        ></TypewriterComponent>
+      </SyntaxHighlighter>
+    </div>
+  );
+};
