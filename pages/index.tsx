5a849609520f6eace6d365c13e66a05aae2de413
Emelit0 <stollk811@gmail.com>
2023-02-13 22:54:49 +0100

    add Name and bio to hompage

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { ReactElement } from "react";
 import styles from "../styles/Home.module.css";
 import CodeBlock from "../components/CodeBlock";
 import TypewriterBlock from "../components/TypewriterBlock";
+import Link from "next/link";
 
 const Home: React.FC = (): ReactElement => {
   return (
@@ -14,6 +15,22 @@ const Home: React.FC = (): ReactElement => {
             <span>
               <TypewriterBlock />
             </span>
+          </div>
+          <div className={styles.background}>
+            <h1>I BUILD</h1>
+            <h1>WEBSITES</h1>
+          </div>
+          <div className={styles.foreground}>
+            <div className={styles.content}>
+              <h1 className={styles.name}>Emelito Pro</h1>
+              <h6 className={styles.bio}>Full Stack Web Developer</h6>
+              <Link href="/projects">
+                <button className={styles.button}>View Work</button>
+              </Link>
+              <Link href="/contact">
+                <button className={styles.outlined}>Contact Me</button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
