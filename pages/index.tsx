b17214d73ae27bbe513c68d1afc52e14633ffab9
Emelit0 <stollk811@gmail.com>
2023-02-14 23:39:36 +0100

    add Name to foreground

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,13 +17,13 @@ const Home: React.FC = (): ReactElement => {
             </span>
           </div>
           <div className={styles.background}>
-            <h1>I BUILD</h1>
-            <h1>WEBSITES</h1>
+            <h1></h1>
+            <h1></h1>
           </div>
           <div className={styles.foreground}>
             <div className={styles.content}>
-              <h1 className={styles.name}>Emelito Pro</h1>
-              <h6 className={styles.bio}>Full Stack Web Developer</h6>
+              <h1 className={styles.name}>Kevin Stoll</h1>
+              <h6 className={styles.bio}>Stackoverflow user</h6>
               <Link href="/projects">
                 <button className={styles.button}>View Work</button>
               </Link>
