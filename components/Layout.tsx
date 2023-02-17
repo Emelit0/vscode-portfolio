def2b38473f9ea1997839d99475051a8999f3b66
Emelit0 <stollk811@gmail.com>
2023-02-17 23:35:04 +0100

    remove image

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,11 +5,13 @@ import styles from "../styles/Layout.module.css";
 import Footer from "./Footer";
 import TabBar from "./TabBar";
 import Terminal from "./Terminal";
+import NeonSign from "./NeonSign";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Titlebar />
+      <NeonSign />
       <div className={styles.main}>
         <Sidebar />
         <Explorer />
