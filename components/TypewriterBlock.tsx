5b962574d93ebc4279795a2611a28d240af3fbd9
Emelit0 <stollk811@gmail.com>
2023-02-17 23:35:36 +0100

    change speed

--- components/TypewriterBlock.tsx
+++ components/TypewriterBlock.tsx
@@ -108,189 +108,189 @@ const TypewriterBlock: React.FC = (): ReactElement => {
   useEffect(() => {
     const timeout = setTimeout(() => {
       setText1(text01.slice(0, text1.length + 1));
-    }, 50);
+    }, 20);
     return () => clearTimeout(timeout);
   }, [text1]);
 
   setTimeout(() => {
     if (text1 === text01) setText2(text02.slice(0, text2.length + 1));
     else return;
-  }, 50);
+  }, 20);
 
   setTimeout(() => {
     if (text2 === text02) setText3(text03.slice(0, text3.length + 1));
     else return;
-  }, 50);
+  }, 20);
 
   setTimeout(() => {
     if (text3 === text03) setText4(text04.slice(0, text4.length + 1));
     else return;
-  }, 50);
+  }, 20);
 
   setTimeout(() => {
     if (text4 === text04) setText5(text05.slice(0, text5.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text5 === text05) setText6(text06.slice(0, text6.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text6 === text06) setText7(text07.slice(0, text7.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text7 === text07) setText8(text08.slice(0, text8.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text8 === text08) setText9(text09.slice(0, text9.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text9 === text09) setText10(text010.slice(0, text10.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text10 === text010) setText11(text011.slice(0, text11.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text11 === text011) setText12(text012.slice(0, text12.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text12 === text012) setText13(text013.slice(0, text13.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text13 === text013) setText14(text014.slice(0, text14.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text14 === text014) setText15(text015.slice(0, text15.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text15 === text015) setText16(text016.slice(0, text16.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text16 === text016) setText17(text017.slice(0, text17.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text17 === text017) setText18(text018.slice(0, text18.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text18 === text018) setText19(text019.slice(0, text19.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text19 === text019) setText20(text020.slice(0, text20.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text20 === text020) setText21(text021.slice(0, text21.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text21 === text021) setText22(text022.slice(0, text22.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text22 === text022) setText23(text023.slice(0, text23.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text23 === text023) setText24(text024.slice(0, text24.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text24 === text024) setText25(text025.slice(0, text25.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text25 === text025) setText26(text026.slice(0, text26.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text26 === text026) setText27(text027.slice(0, text27.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text27 === text027) setText28(text028.slice(0, text28.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text28 === text028) setText29(text029.slice(0, text29.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text29 === text029) setText30(text030.slice(0, text30.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text30 === text030) setText31(text031.slice(0, text31.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text31 === text031) setText32(text032.slice(0, text32.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text32 === text032) setText33(text033.slice(0, text33.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text33 === text033) setText34(text034.slice(0, text34.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text34 === text034) setText35(text035.slice(0, text35.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text35 === text035) setText36(text036.slice(0, text36.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text36 === text036) setText37(text037.slice(0, text37.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text37 === text037) setText38(text038.slice(0, text38.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text38 === text038) setText39(text039.slice(0, text39.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text39 === text039) setText40(text040.slice(0, text40.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text40 === text040) setText41(text041.slice(0, text41.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text41 === text041) setText42(text042.slice(0, text42.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text42 === text042) setText43(text043.slice(0, text43.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text43 === text043) setText44(text044.slice(0, text44.length + 1));
     else return;
-  }, 50);
+  }, 20);
   setTimeout(() => {
     if (text44 === text044) setText45(text045.slice(0, text45.length + 1));
     else return;
-  }, 50);
+  }, 20);
 
   return (
     <>
