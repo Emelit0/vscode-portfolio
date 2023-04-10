0a9ee2d7be9312eb4b07e93e969d806a82048ac9
Emelit0 <stollk811@gmail.com>
2023-04-10 23:54:28 +0200

    add icons and about test

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,17 @@
 const AboutPage = () => {
-  return <h3>A Little Bit About Me</h3>;
+  return (
+    <>
+      {" "}
+      <h3>A Little Bit About Me</h3>
+      <p>
+        Hi there! My name is Kevin Stoll, and I am currently studying Applied
+        Computer Science. I have a passion for technology and have always been
+        fascinated by how computers work. In addition to my studies, I am proud
+        to work for Cisco, a company that has been at the forefront of the
+        technology industry for many years.
+      </p>
+    </>
+  );
 };
 
 export async function getStaicProps() {
